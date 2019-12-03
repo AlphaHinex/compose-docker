57ab7eeb2d5dedff3bf28c16355701c57302a855
AlphaHinex <AlphaHinex@gmail.com>
2019-12-03 14:21:35 +0800

    Add entrypoint

---
+++ teamcity/server/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+mkdir -p $TEAMCITY_DATA_PATH/lib/jdbc $TEAMCITY_DATA_PATH/config $TEAMCITY_DATA_PATH/plugins
+
+if [ ! -f "$TEAMCITY_DATA_PATH/lib/jdbc/mysql-connector-java-5.1.48.jar" ];
+then
+    echo "Downloading mysql JDBC driver..."
+    curl -o $TEAMCITY_DATA_PATH/lib/jdbc/mysql-connector-java-5.1.48.jar https://repo.gradle.org/gradle/libs/mysql/mysql-connector-java/5.1.48/mysql-connector-java-5.1.48.jar
+fi
+
+if [ -d "/opt/teamcity/webapps/ROOT" ];
+then
+    # Move ROOT to use custom context
+    mv /opt/teamcity/webapps/ROOT /opt/teamcity/webapps/$TEAMCITY_CONTEXT
+fi
+
+echo "Starting teamcity..."
+exec /opt/teamcity/bin/teamcity-server.sh run
